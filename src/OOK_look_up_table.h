41e260e2ecd41cbd17d0a6d9cec0d7b169144fd8
Aidan Wilson <aidan@Aidans-MacBook-Pro.local>
2020-02-10 23:13:17 +0000

    Adding the Look up table

---
+++ src/OOK_look_up_table.h
@@ -0,0 +1,28 @@
+char A=1; 
+char B=2;
+char C=3;
+char D=4;
+char E=5;
+char F=6;
+char G=7;
+char H=8;
+char I=9;
+char J=10;
+char K=11;
+char L=12;
+char M=13;
+char N=14;
+char O=15;
+char P=16;
+char Q=17;
+char R=18;
+char S=19;
+char T=20;
+char U=21;
+char V=22;
+char W=23;
+char X=24;
+char Y=25;
+char Z=26;
+
+
