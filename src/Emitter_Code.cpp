fcdb9c27da7081e165a55532cbce4bbb72cebf2d
Aidan Wilson <aidan@Aidans-MacBook-Pro.local>
2020-02-10 23:12:35 +0000

    Setting Pins

--- src/Emitter_Code.cpp
+++ src/Emitter_Code.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
 
+
+int LED_Driving_pin=7;
+
 void setup() {
-  // put your setup code here, to run once:
-}
+  Serial.begin(9600);
+  pinMode(LED_Driving_pin, OUTPUT);
+  }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //digitalWrite(LED_Driving_pin, HIGH);
+  //delay();
+  digitalWrite(LED_Driving_pin, LOW);
+  //delay();
 }
