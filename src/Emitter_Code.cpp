a81b47a2509ce564dd04510ae78995cc1b84c1e7
Aidan Wilson <aidan@Aidans-MacBook-Pro.local>
2020-02-11 00:20:02 +0000

    BDC

--- src/Emitter_Code.cpp
+++ src/Emitter_Code.cpp
@@ -1,7 +1,10 @@
 #include <Arduino.h>
-
+#include <OOK_look_up_table.h>
 
 int LED_Driving_pin=7;
+
+//Hello = 8,5,12,12,15
+//World = 23,15,18,12,4
 
 void setup() {
   Serial.begin(9600);
@@ -9,8 +12,20 @@ void setup() {
   }
 
 void loop() {
+
+
+
   //digitalWrite(LED_Driving_pin, HIGH);
   //delay();
-  digitalWrite(LED_Driving_pin, LOW);
+  //digitalWrite(LED_Driving_pin, LOW);
   //delay();
 }
+/*
+void BDC(){
+  int a[]={H, E, L, L, O, W, O, R, L, D};
+  int b[]={};
+
+for (int i=0; a[].length; i++){
+  b[i]=a[i]%2;
+}
+}*/
